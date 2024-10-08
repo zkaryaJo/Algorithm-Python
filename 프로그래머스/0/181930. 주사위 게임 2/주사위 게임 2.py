43c1cf0c1928f46f74c6eb2b9e7cb73b848c4772
zkaryaJo <46834158+zkaryaJo@users.noreply.github.com>
2024-10-08 15:17:49 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,9 +1,20 @@
 def solution(a, b, c):
-    answer = 0
-    if a==b==c: #모두 같은 경우
+    # answer = 0
+    # if a==b==c: #모두 같은 경우
+    #     answer = (a + b + c) * (a**2 + b**2 + c**2 ) * (a**3 + b**3 + c**3 )
+    # elif a !=b and a!=c and b!=c : # 모두 다른경우
+    #     answer = a+b+c
+    # else:
+    #     answer = (a + b + c) * (a**2 + b**2 + c**2 )
+    # return answer
+    
+    dup = len(set([a,b,c]))
+    
+    if dup == 1:
         answer = (a + b + c) * (a**2 + b**2 + c**2 ) * (a**3 + b**3 + c**3 )
-    elif a !=b and a!=c and b!=c : # 모두 다른경우
-        answer = a+b+c
-    else:
+    elif dup == 2:
         answer = (a + b + c) * (a**2 + b**2 + c**2 )
+    else:
+        answer = a+b+c
+        
     return answer
