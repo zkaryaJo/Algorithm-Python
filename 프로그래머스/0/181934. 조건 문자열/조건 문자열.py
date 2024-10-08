b738ef048309ffdb7a7a4005cb113ee4e8d04a20
zkaryaJo <46834158+zkaryaJo@users.noreply.github.com>
2024-10-08 13:54:19 +0900

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -1,28 +1,30 @@
 def solution(ineq, eq, n, m):
     
-    if(eq == '=') :
-        if(n == m) :
-            return 1
-        else :
-            if(ineq =='>'):
-                if(n>m):
-                    return 1
-                else:
-                    return 0
-            else:
-                if(n<m):
-                    return 1
-                else:
-                    return 0
-    else:
-        if(ineq =='>'):
-            if(n>m):
-                return 1
-            else:
-                return 0
-        else:
-            if(n<m):
-                return 1
-            else:
-                return 0
+    return int(eval(str(n) +ineq + eq.replace('!','') + str(m)))
+    
+    # if(eq == '=') :
+    #     if(n == m) :
+    #         return 1
+    #     else :
+    #         if(ineq =='>'):
+    #             if(n>m):
+    #                 return 1
+    #             else:
+    #                 return 0
+    #         else:
+    #             if(n<m):
+    #                 return 1
+    #             else:
+    #                 return 0
+    # else:
+    #     if(ineq =='>'):
+    #         if(n>m):
+    #             return 1
+    #         else:
+    #             return 0
+    #     else:
+    #         if(n<m):
+    #             return 1
+    #         else:
+    #             return 0
           
