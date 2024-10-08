377dd0e9b761bd863260aa59d961fa0ea7ee505e
zkaryaJo <46834158+zkaryaJo@users.noreply.github.com>
2024-10-08 10:59:19 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -3,7 +3,7 @@ def solution(n):
     answer = 0
     
     if n%2:
-        answer = sum(i for i in range(1, n+1, 2))
+        answer = sum(range(1, n+1, 2))
     else:
         answer = sum(i*i for i in range(0, n+1, 2))
     
