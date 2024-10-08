364c9d32fda0328030882852a6c20da839f0575f
zkaryaJo <46834158+zkaryaJo@users.noreply.github.com>
2024-10-08 14:21:18 +0900

    [level 0] Title: 코드 처리하기, Time: 11.59 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -6,11 +6,9 @@ def solution(code):
         if(code[i] == '1') :
             mode = not mode
         else :
-            if(mode):
-                if(i%2==1):
-                    answer += code[i]
-            else:
-                if(i%2==0):
-                    answer += code[i]    
+            if(mode and i%2==1):
+                answer += code[i]
+            elif(not mode and i%2==0):
+                answer += code[i]    
     
     return 'EMPTY' if answer == '' else answer;
