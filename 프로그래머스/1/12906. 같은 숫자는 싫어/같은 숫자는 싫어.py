0592f2ceada2b5af3f07985eefeecdf457715b53
zkaryaJo <46834158+zkaryaJo@users.noreply.github.com>
2024-10-14 14:57:52 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 115.54 ms, Memory: 27.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    answer = []
+    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    
+    
+    for item in arr:
+        if(len(answer) == 0):
+            answer.append(item)
+        elif answer[-1] != item:
+            answer.append(item)
+    
+    return answer
